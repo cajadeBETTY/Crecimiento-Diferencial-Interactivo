e1240ad1260bac8b4a6826f185e32a7a07030e25
Estudio C.A.J.A.S. <bchavez@estudioencajas.com>
2025-04-22 22:04:27 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,128 +31,112 @@ let frecuenciaHistorial = 10;
 
 let inputFrecuenciaHistorial;
 let inputMinDist, inputMaxDist;
-
 let inputMaxPoints;
-let btnExportPNG;
+let btnExportPNG, btnExportSVG;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+// setup() y iniciarCrecimiento() aquí...
 
-  inputMinDist = select('#inputMinDist');
-  inputMaxDist = select('#inputMaxDist');
-  inputMaxPoints = select('#inputMaxPoints');
-  inputFrecuenciaHistorial = select('#inputFrecuenciaHistorial');
-  inputPuntos = select('#inputPuntos');
+function draw() {
+  background(255);
+  push();
 
-  inputFrecuenciaHistorial.input(() => {
-    frecuenciaHistorial = int(inputFrecuenciaHistorial.value());
-  });
+  translate(width / 2 + offsetX, height / 2 + offsetY);
+  scale(zoom);
+  translate(-width / 2, -height / 2);
+  maxPoints = int(inputMaxPoints.value());
 
-  sliderRadio = select('#sliderRadio');
-  radioValorSpan = select('#radioValor');
-  sliderRadio.input(() => {
-    radioValorSpan.html(sliderRadio.value());
-  });
+  let tipoVisual = tipoVisualSelect.value();
 
-  tipoRuidoSelect = select('#tipoRuido');
-  sliderAmplitud = select('#sliderAmplitud');
-  sliderFrecuencia = select('#sliderFrecuencia');
-  valorAmplitudSpan = select('#valorAmplitud');
-  valorFrecuenciaSpan = select('#valorFrecuencia');
-
-  sliderAmplitud.input(() => {
-    valorAmplitudSpan.html(sliderAmplitud.value());
-  });
-  sliderFrecuencia.input(() => {
-    valorFrecuenciaSpan.html(sliderFrecuencia.value());
-  });
-
-  sliderRepulsion = select('#sliderRepulsion');
-  valorRepulsionSpan = select('#valorRepulsion');
-  sliderRepulsion.input(() => {
-    valorRepulsionSpan.html(sliderRepulsion.value());
-  });
-
-  tipoVisualSelect = select('#tipoVisual');
-  toggleHistorialBtn = select('#toggleHistorialBtn');
-  toggleNodosBtn = select('#toggleNodosBtn');
-  clearHistorialBtn = select('#clearHistorialBtn');
-
-  toggleHistorialBtn.mousePressed(() => {
-    mostrarHistorial = !mostrarHistorial;
-    toggleHistorialBtn.html(mostrarHistorial ? "🕘 Ocultar historial" : "🕘 Ver historial");
-  });
-
-  toggleNodosBtn.mousePressed(() => {
-    mostrarNodos = !mostrarNodos;
-    toggleNodosBtn.html(mostrarNodos ? "🔘 Ocultar nodos" : "🔘 Mostrar nodos");
-  });
-
-  clearHistorialBtn.mousePressed(() => {
-    historialFormas = [];
-    frameHistorial = 0;
-  });
-
-  playPauseBtn = select('#playPauseBtn');
-  restartBtn = select('#restartBtn');
-  playPauseBtn.mousePressed(togglePlayPause);
-  restartBtn.mousePressed(reiniciarCrecimiento);
-
-  btnExportPNG = select('#btnExportPNG');
-  btnExportPNG.mousePressed(() => {
-    saveCanvas('crecimiento_diferencial', 'png');
-  });
-
-  noFill();
-}
-
-function togglePlayPause() {
-  if (!iniciado) {
-    iniciarCrecimiento();
-    playPauseBtn.html('⏸ Pausar');
-  } else {
-    running = !running;
-    playPauseBtn.html(running ? '⏸ Pausar' : '▶ Reanudar');
-  }
-}
-
-function iniciarCrecimiento() {
-  let cantidad = int(inputPuntos.value());
-  radio = float(sliderRadio.value());
-
-  if (isNaN(cantidad) || cantidad < 3 || isNaN(radio) || radio <= 0) {
-    alert("Por favor ingresa valores válidos.");
-    return;
+  if (mostrarHistorial && historialFormas.length > 0) {
+    stroke(180);
+    strokeWeight(1 / zoom);
+    noFill();
+    for (let forma of historialFormas) {
+      beginShape();
+      for (let p of forma) {
+        if (tipoVisual === 'curva') {
+          curveVertex(p.x, p.y);
+        } else {
+          vertex(p.x, p.y);
+        }
+      }
+      if (tipoVisual === 'curva') {
+        curveVertex(forma[0].x, forma[0].y);
+        curveVertex(forma[0].x, forma[0].y);
+      }
+      endShape(CLOSE);
+    }
   }
 
-  let tipo = tipoRuidoSelect.value();
-  let amp = float(sliderAmplitud.value());
-  let freq = float(sliderFrecuencia.value());
+  if (points.length > 0) {
+    stroke(0);
+    strokeWeight(1 / zoom);
+    noFill();
 
-  let minInput = float(inputMinDist.value());
-  let maxInput = float(inputMaxDist.value());
+    beginShape();
+    if (tipoVisual === 'curva') {
+      curveVertex(points[0].x, points[0].y);
+      curveVertex(points[0].x, points[0].y);
+      for (let p of points) {
+        curveVertex(p.x, p.y);
+      }
+      curveVertex(points[0].x, points[0].y);
+      curveVertex(points[0].x, points[0].y);
+    } else {
+      for (let p of points) {
+        vertex(p.x, p.y);
+      }
+    }
+    endShape(CLOSE);
 
-  let circunferencia = TWO_PI * radio;
-  let distInicial = circunferencia / cantidad;
+    if (mostrarNodos) {
+      fill(0);
+      noStroke();
+      for (let p of points) {
+        circle(p.x, p.y, 4 / zoom);
+      }
+    }
+  }
+  pop();
 
-  minDist = (!isNaN(minInput) && minInput > 0) ? minInput : distInicial * 1.2;
-  maxDist = (!isNaN(maxInput) && maxInput > 0) ? maxInput : distInicial * 1.2;
+  if (!running || points.length >= maxPoints) return;
 
-  points = [];
+  if (mostrarHistorial && frameHistorial % frecuenciaHistorial === 0) {
+    historialFormas.push(points.map(p => createVector(p.x, p.y)));
+  }
+  frameHistorial++;
 
-  for (let i = 0; i < cantidad; i++) {
-    let angle = TWO_PI * i / cantidad;
-    let x = width / 2 + radio * cos(angle);
-    let y = height / 2 + radio * sin(angle);
+  let nuevosPuntos = [];
+  for (let i = 0; i < points.length; i++) {
+    let actual = points[i];
+    let fuerza = createVector(0, 0);
+    let cercanos = 0;
 
+    for (let j = 0; j < points.length; j++) {
+      if (i !== j) {
+        let otro = points[j];
+        let d = dist(actual.x, actual.y, otro.x, otro.y);
+        if (d < minDist) {
+          let dir = p5.Vector.sub(actual, otro);
+          dir.normalize();
+          dir.mult(float(sliderRepulsion.value()) / d);
+          fuerza.add(dir);
+          cercanos++;
+        }
+      }
+    }
+
+    let tipo = tipoRuidoSelect.value();
+    let amp = float(sliderAmplitud.value());
+    let freq = float(sliderFrecuencia.value());
     let ruido = createVector(0, 0);
+
     if (tipo === 'perlin') {
-      let n = noise(x * freq, y * freq);
-      let angleOffset = n * TWO_PI;
-      ruido = p5.Vector.fromAngle(angleOffset).mult(amp);
+      let n = noise(actual.x * freq, actual.y * freq + noiseOffset);
+      ruido = p5.Vector.fromAngle(n * TWO_PI).mult(amp);
     } else if (tipo === 'perlinImproved') {
-      let nx = noise(x * freq);
-      let ny = noise(y * freq);
+      let nx = noise(actual.x * freq, noiseOffset);
+      let ny = noise(actual.y * freq, noiseOffset + 1000);
       ruido = createVector((nx - 0.5) * amp * 2, (ny - 0.5) * amp * 2);
     } else if (tipo === 'valor') {
       ruido = createVector(random(-1, 1) * amp, random(-1, 1) * amp);
@@ -160,12 +144,92 @@ function iniciarCrecimiento() {
       ruido = p5.Vector.random2D().mult(amp);
     }
 
-    x += ruido.x;
-    y += ruido.y;
+    fuerza.div(max(1, cercanos)).add(ruido);
+    actual.add(fuerza);
+    nuevosPuntos.push(actual);
 
-    points.push(createVector(x, y));
+    let siguiente = points[(i + 1) % points.length];
+    if (p5.Vector.dist(actual, siguiente) > maxDist) {
+      nuevosPuntos.push(p5.Vector.add(actual, siguiente).div(2));
+    }
+  }
+  points = nuevosPuntos;
+  noiseOffset += 0.01;
+}
+
+function mouseWheel(event) {
+  zoom *= 1 - event.delta * 0.001;
+  return false;
+}
+
+function mousePressed() {
+  if (mouseButton === LEFT) {
+    isDragging = true;
+    lastMouseX = mouseX;
+    lastMouseY = mouseY;
+  }
+}
+
+function mouseReleased() {
+  isDragging = false;
+}
+
+function mouseDragged() {
+  if (isDragging) {
+    let dx = mouseX - lastMouseX;
+    let dy = mouseY - lastMouseY;
+    offsetX += dx;
+    offsetY += dy;
+    lastMouseX = mouseX;
+    lastMouseY = mouseY;
+  }
+}
+
+function exportarSVG() {
+  let svgCanvas = createGraphics(windowWidth, windowHeight, SVG);
+  svgCanvas.translate(width / 2 + offsetX, height / 2 + offsetY);
+  svgCanvas.scale(zoom);
+  svgCanvas.translate(-width / 2, -height / 2);
+  svgCanvas.strokeWeight(1 / zoom);
+  svgCanvas.noFill();
+
+  const tipoVisual = tipoVisualSelect.value();
+
+  if (mostrarHistorial) {
+    svgCanvas.stroke(180);
+    for (let forma of historialFormas) {
+      svgCanvas.beginShape();
+      for (let p of forma) {
+        tipoVisual === 'curva' ? svgCanvas.curveVertex(p.x, p.y) : svgCanvas.vertex(p.x, p.y);
+      }
+      if (tipoVisual === 'curva') {
+        svgCanvas.curveVertex(forma[0].x, forma[0].y);
+        svgCanvas.curveVertex(forma[0].x, forma[0].y);
+      }
+      svgCanvas.endShape(CLOSE);
+    }
   }
 
-  iniciado = true;
-  running = true;
+  if (points.length > 0) {
+    svgCanvas.stroke(0);
+    svgCanvas.beginShape();
+    if (tipoVisual === 'curva') {
+      svgCanvas.curveVertex(points[0].x, points[0].y);
+      svgCanvas.curveVertex(points[0].x, points[0].y);
+      for (let p of points) svgCanvas.curveVertex(p.x, p.y);
+      svgCanvas.curveVertex(points[0].x, points[0].y);
+      svgCanvas.curveVertex(points[0].x, points[0].y);
+    } else {
+      for (let p of points) svgCanvas.vertex(p.x, p.y);
+    }
+    svgCanvas.endShape(CLOSE);
+
+    if (mostrarNodos) {
+      svgCanvas.noStroke();
+      svgCanvas.fill(0);
+      for (let p of points) svgCanvas.circle(p.x, p.y, 4 / zoom);
+    }
+  }
+
+  svgCanvas.save('crecimiento_diferencial.svg');
 }
