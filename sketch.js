d4139a13b098bbef8b13e1be1e63b1b7a3bf46c0
Estudio C.A.J.A.S. <bchavez@estudioencajas.com>
2025-04-27 22:21:52 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -289,7 +289,7 @@ if (tipoVisualSelect.value() === 'curva') {
     points=nuevos; noiseOffset+=0.01;
   }
 // dimensiones del logo (manteniendo el aspect)
-const margin     = 40;
+const margin     = 30;
 const maxLogoW   = 750;
 const aspect     = logoImg.width / logoImg.height;
 const w          = maxLogoW;
